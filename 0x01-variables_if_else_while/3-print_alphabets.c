1e3d19b51f25de6140073b99344db4bf37529f0a
Some969 <someisaac21@gmail.com>
2023-06-16 13:51:52 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-print_alphabets.c -o 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char e;
+
+	char c;
+
+	e = 'a';
+	c = 'A';
+	while
+		(e <= 'z') {
+			putchar(e);
+			e++;
+	}
+	while
+		(c <= 'Z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
