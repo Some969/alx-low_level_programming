e5b85a9b44331a1b0184e65d97bce69a667d5f68
Some969 <someisaac21@gmail.com>
2023-06-16 14:45:58 -0700

    no 6

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	char e;
+
+	e = 'a';
+	for (e < == 'z' && e !'q''e');
+		putchar(e);
+	e++;
+		putchar('\n');
+		return (0);
+}
