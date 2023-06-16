c5c544112bc51ef6fad82483f4857c0c17eea54e
Some969 <someisaac21@gmail.com>
2023-06-16 15:59:22 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int v;
+	int u;
+
+	for (v = '0'; v < '9'; v++)
+	{
+	for (u = v + 1; u <= '9'; u++)
+	{
+	if (u != v)
+	{
+	putchar(v);
+	putchar(u);
+	if (v == '8' && u == '9')
+	continue;
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
